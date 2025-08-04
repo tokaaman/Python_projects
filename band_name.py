bcc5c5b0c90d8a7a88f83a5e7dbde527960e538b
Tokuma Aman <tokuma.amani@gmail.com>
2025-08-04 10:47:18 +0300

    Add files via upload

---
+++ band_name.py
@@ -0,0 +1,7 @@
+print("Welcome to bandName genrator")
+city_name=input("What is the name of the city you grow in")
+print(city_name)
+pet_name=input("Ehat is your pet's name")
+print(pet_name)
+band_name= city_name+pet_name
+print("your brand new band name is  :" +band_name) 
